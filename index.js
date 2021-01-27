518c6349e1ff3700314fb3fa6b0a9489b8d9807e
Arbuzik999 <cheremuxin.v64@gmail.com>
2021-01-28 02:01:54 +0300

    minor changes

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ function goBack(path) {
 }
 
 function doSecret() {
-    $(".secret").one("click", function(e) {
-        e.preventDefault();
-        
-        $("body").css({ backgroundColor: "#ff0000"})
-        $(".text").css({ color: "#ffffff"})
-        $(".text p, .secret span").text("ВЫХОДА НЕТ")
-        
-        $("#logo").animate({ opacity: 0}, 1000)
-        $("#footer").animate({ opacity: 0}, 1000)
-        $("").anim
-    })
+//    $(".secret").one("click", function(e) {
+//        e.preventDefault();
+//        
+//        $("body").css({ backgroundColor: "#ff0000"})
+//        $(".text").css({ color: "#ffffff"})
+//        $(".text p, .secret span").text("ВЫХОДА НЕТ")
+//        
+//        $("#logo").animate({ opacity: 0}, 1000)
+//        $("#footer").animate({ opacity: 0}, 1000)
+//        $("").anim
+//    })
 }
 
 function prikol() {
-    var rick = $("#rick")
-    rick.prop("volume", 0.1)
-    rick.trigger("play")
+//    var rick = $("#rick")
+//    rick.prop("volume", 0.1)
+//    rick.trigger("play")
 }
