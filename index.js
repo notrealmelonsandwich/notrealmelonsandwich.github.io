7af8ae45bec79f6a71ebc554ee4f74d7c92684e2
Arbuzik999 <cheremuxin.v64@gmail.com>
2021-01-28 01:48:34 +0300

    minor fixes

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function goBack(path) {
     this.window.location.replace(path);
 }
+
+function doSecret() {
+    $(".secret").one("click", function(e) {
+        e.preventDefault();
+        
+        $("body").css({ backgroundColor: "#ff0000"})
+        $(".text").css({ color: "#ffffff"})
+        $(".text p, .secret span").text("ВЫХОДА НЕТ")
+        
+        $("#logo").animate({ opacity: 0}, 1000)
+        $("#footer").animate({ opacity: 0}, 1000)
+        $("").anim
+    })
+}
+
+function prikol() {
+    var rick = $("#rick")
+    rick.prop("volume", 0.1)
+    rick.trigger("play")
+}
